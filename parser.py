85635f4831eda4213121d5e3024125fdc21a8230
Nick Levesque <nick@levesque.io>
2016-09-14 20:21:22 -0400

    Refactor to use SyntaxException instead of passing False around. Lots of progress on parsing instructions and assembling into bits.

--- parser.py
+++ parser.py
@@ -5,6 +5,9 @@ import re
 # if a label is on the same line as an instruction, use that instruction number
 # otherwise, find the next instruction and use that number.
 
+class SyntaxException(Exception):
+    pass
+
 class Parser():
     patterns = {
         'instruction':  r'^(?:[\s]*.+:)*[\s]*(?P<instruction>[a-zA-Z0-9]+?)(?!:)[\s]',
@@ -57,30 +60,30 @@ class Parser():
 
     instructions = {
         # r types
-        'sll':   {'type':'r', 'func':0x00, 'syntax':('rd', 'rt', 'shamt')},
-        'srl':   {'type':'r', 'func':0x02, 'syntax':('rd', 'rt', 'shamt')},
-        'sra':   {'type':'r', 'func':0x03, 'syntax':('rd', 'rt', 'shamt')},
-        'sllv':  {'type':'r', 'func':0x04, 'syntax':('rd', 'rt', 'rs')},
-        'srlv':  {'type':'r', 'func':0x06, 'syntax':('rd', 'rt', 'rs')},
-        'srav':  {'type':'r', 'func':0x07, 'syntax':('rd', 'rt', 'rs')},
-        'jr':    {'type':'r', 'func':0x08, 'syntax':('rs')},
-        'mfhi':  {'type':'r', 'func':0x10, 'syntax':('rd')},
-        'mflo':  {'type':'r', 'func':0x12, 'syntax':('rd')},
-        'mult':  {'type':'r', 'func':0x18, 'syntax':('rs', 'rt')},
-        'multu': {'type':'r', 'func':0x19, 'syntax':('rs', 'rt')},
-        'div':   {'type':'r', 'func':0x1A, 'syntax':('rs', 'rt')},
-        'divu':  {'type':'r', 'func':0x1B, 'syntax':('rs', 'rt')},
-        'add':   {'type':'r', 'func':0x20, 'syntax':('rd', 'rs', 'rt')},
-        'addu':  {'type':'r', 'func':0x21, 'syntax':('rd', 'rs', 'rt')},
-        'sub':   {'type':'r', 'func':0x22, 'syntax':('rd', 'rs', 'rt')},
-        'subu':  {'type':'r', 'func':0x23, 'syntax':('rd', 'rs', 'rt')},
-        'and':   {'type':'r', 'func':0x24, 'syntax':('rd', 'rs', 'rt')},
-        'or':    {'type':'r', 'func':0x25, 'syntax':('rd', 'rs', 'rt')},
-        'xor':   {'type':'r', 'func':0x26, 'syntax':('rd', 'rs', 'rt')},
-        'nor':   {'type':'r', 'func':0x27, 'syntax':('rd', 'rs', 'rt')},
-        'slt':   {'type':'r', 'func':0x2A, 'syntax':('rd', 'rs', 'rt')},
-        'sltu':  {'type':'r', 'func':0x2B, 'syntax':('rd', 'rs', 'rt')},
-        'mfc0':  {'type':'r', 'op':0x16, 'syntax':('rd', 'rs')},
+        'sll':   {'type':'r', 'func':0x00, 'op':0, 'syntax':('rd', 'rt', 'shamt')},
+        'srl':   {'type':'r', 'func':0x02, 'op':0, 'syntax':('rd', 'rt', 'shamt')},
+        'sra':   {'type':'r', 'func':0x03, 'op':0, 'syntax':('rd', 'rt', 'shamt')},
+        'sllv':  {'type':'r', 'func':0x04, 'op':0, 'syntax':('rd', 'rt', 'rs')},
+        'srlv':  {'type':'r', 'func':0x06, 'op':0, 'syntax':('rd', 'rt', 'rs')},
+        'srav':  {'type':'r', 'func':0x07, 'op':0, 'syntax':('rd', 'rt', 'rs')},
+        'jr':    {'type':'r', 'func':0x08, 'op':0, 'syntax':('rs')},
+        'mfhi':  {'type':'r', 'func':0x10, 'op':0, 'syntax':('rd')},
+        'mflo':  {'type':'r', 'func':0x12, 'op':0, 'syntax':('rd')},
+        'mult':  {'type':'r', 'func':0x18, 'op':0, 'syntax':('rs', 'rt')},
+        'multu': {'type':'r', 'func':0x19, 'op':0, 'syntax':('rs', 'rt')},
+        'div':   {'type':'r', 'func':0x1A, 'op':0, 'syntax':('rs', 'rt')},
+        'divu':  {'type':'r', 'func':0x1B, 'op':0, 'syntax':('rs', 'rt')},
+        'add':   {'type':'r', 'func':0x20, 'op':0, 'syntax':('rd', 'rs', 'rt')},
+        'addu':  {'type':'r', 'func':0x21, 'op':0, 'syntax':('rd', 'rs', 'rt')},
+        'sub':   {'type':'r', 'func':0x22, 'op':0, 'syntax':('rd', 'rs', 'rt')},
+        'subu':  {'type':'r', 'func':0x23, 'op':0, 'syntax':('rd', 'rs', 'rt')},
+        'and':   {'type':'r', 'func':0x24, 'op':0, 'syntax':('rd', 'rs', 'rt')},
+        'or':    {'type':'r', 'func':0x25, 'op':0, 'syntax':('rd', 'rs', 'rt')},
+        'xor':   {'type':'r', 'func':0x26, 'op':0, 'syntax':('rd', 'rs', 'rt')},
+        'nor':   {'type':'r', 'func':0x27, 'op':0, 'syntax':('rd', 'rs', 'rt')},
+        'slt':   {'type':'r', 'func':0x2A, 'op':0, 'syntax':('rd', 'rs', 'rt')},
+        'sltu':  {'type':'r', 'func':0x2B, 'op':0, 'syntax':('rd', 'rs', 'rt')},
+        'mfc0':  {'type':'r', 'op':0x16, 'op':0, 'syntax':('rd', 'rs')},
         # i types
         'beq':   {'type':'i', 'op':0x04, 'syntax':('rt', 'rs', 'i')},
         'bne':   {'type':'i', 'op':0x05, 'syntax':('rt', 'rs', 'i')},
@@ -102,6 +105,7 @@ class Parser():
         'jal':   {'type':'j', 'op':0x03},
         }
 
+
     def get_register(self, r):
     '''
     Returns the register number associated with r.
@@ -113,77 +117,113 @@ class Parser():
         else if (int(r) >= 0) and (int(r) <= 31):
             return int(r)
         else:
-            return False
+            raise SyntaxException('Invalid register ' + r + ' on line ' + self.linenum)
+
+
+    def str_to_val(self, valstr, n=32):
+    '''
+    Converts a string to an int, and optionally
+    limits that int to less than 2^n.
+    '''
+        val = int(valstr)
+        if (val < pow(2, n)):
+            return val
+        else:
+            raise SyntaxException('Value ' + valstr + ' larger than ' + str(pow(2, n)))
+
 
     def parse_instruction(self, line):
+        r_groups = ['rt', 'rs', 'rd', 'shamt']
+        i_groups = ['rt', 'rs', 'i']
+        j_groups = ['address']
         # allow for labels on the same line as an instruction.
         pattern = patterns['label'] + patterns['instruction']
         m = re.search(pattern, line)
         i = self.instructions.get(m.group('instruction'))
+        j = 1
         if i is not None:
             for arg in i['syntax']:
                 pattern += patterns[p]
-                if i != len(i['syntax']):
+                if j != len(i['syntax']):
                     pattern += patterns['comma']
-                i += 1
+                j += 1
             pattern += patterns['eol']
                 
             m = re.search(pattern, line)
-            
+            a = {}
+            for arg in i['syntax']:
+                if arg == 'i(rs)':
+                    # special case
+                    try:
+                        a['i'] = m.group['i']
+                    except:
+                        raise SyntaxException('Expected i on line ' + self.linenum)
+                    try:
+                        a['rs'] = m.group['rs']
+                    except:
+                        raise SyntaxException('Expected rs on line ' + self.linenum)
+                else:
+                    try:
+                        a[arg] = m.group[arg]
+                    except:
+                        raise SyntaxException('Expected ' + arg + ' on line ' + self.linenum)
+
+
+            # set unused values to 0,
+            # then assemble bit sequence based on instruction type.
             if i['type'] == 'r':
-                # working on this
-                bits = assemble_rtype(i['func'],
-                                      rs,
-                                      rt,
-                                      rd,
-                                      shamt,
-                                      op)
+                for arg in r_groups:
+                    if arg not in a:
+                        a[arg] = 0
+                bits = assemble_rtype(i['func'], a['rs'], a['rt'], a['rd'], a['shamt'], i['op'])
             elif i['type'] == 'i':
-                bits = assemble_itype()
+                for arg in i_groups:
+                    if arg not in a:
+                        a[arg] = 0
+                bits = assemble_itype(i['op'], a['rs'], a['rt'], a['i'])
             elif i['type'] == 'j':
-                bits = assemble_jtype()
+                for arg in j_groups:
+                    if arg not in a:
+                        a[arg] = 0
+                bits = assemble_jtype(i['op'], a['address'])
             else:
-                raise Exception('Unsupported instruction type.')
-                return
+                raise SyntaxException('Unsupported instruction type.')
             return bits
+
         else:
             return False
 
-    def assemble_rtype(self, func, rs=0, rt=0, rd=0, shamt=0, op=0):
+
+    def assemble_rtype(self, func, rs, rt, rd, shamt, op):
+        rs = get_register(rs)
+        rt = get_register(rt)
+        rd = get_register(rd)
+        shamt = str_to_val(shamt, 5)
         bits = ''
-        bits += '{0:06b}'.format(op)
-        bits += '{0:05b}'.format(rs)
-        bits += '{0:05b}'.format(rt)
-        bits += '{0:05b}'.format(rd)
-        bits += '{0:05b}'.format(shamt)
-        bits += '{0:06b}'.format(func)
+        bits += '{0:6b}'.format(op)
+        bits += '{0:5b}'.format(rs)
+        bits += '{0:5b}'.format(rt)
+        bits += '{0:5b}'.format(rd)
+        bits += '{0:5b}'.format(shamt)
+        bits += '{0:6b}'.format(func)
         return bits
 
-    def assemble_itype(self, op, rt=0, rs=0, i=0):
+
+    def assemble_itype(self, op, rt, rs, i):
+        rt = get_register(rt)
+        rs = get_register(rs)
+        i = str_to_val(i, 16)
         bits = ''
-        bits += '{0:06b}'.format(op)
-        bits += '{0:05b}'.format(rs)
-        bits += '{0:05b}'.format(rt)
-        bits += '{0:016b}'.format(i)
+        bits += '{0:6b}'.format(op)
+        bits += '{0:5b}'.format(rs)
+        bits += '{0:5b}'.format(rt)
+        bits += '{0:16b}'.format(i)
         return bits
 
-    def assemble_jtype(self, op, target):
+
+    def assemble_jtype(self, op, address):
+        address = str_to_val(address, 26)
         bits = ''
-        bits += '{0:06b}'.format(op)
-        bits += '{0:026b}'.format(target)
+        bits += '{0:6b}'.format(op)
+        bits += '{0:26b}'.format(address)
         return bits
-        
-
-pattern = r''
-addiu_syntax = i_types['addiu'][2]
-i = 1
-for p in addiu_syntax:
-    pattern += patterns[p]
-    if i != len(addiu_syntax):
-        pattern += patterns['comma']
-    i += 1
-pattern += patterns['eol']
-
-print(pattern)
-
-
