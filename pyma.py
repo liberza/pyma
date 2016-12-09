1e67341863a4228ae70ca40c9194410b1beaa8b7
Nick Levesque <nick@levesque.io>
2016-12-09 11:36:00 -0500

    allow files to be passed as args

--- pyma.py
+++ pyma.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 
 from parser import Parser
+import sys
 
 def hexstr(bits):
     h = int(bits, 2)
     return '{0:08x}'.format(h)
 
+# Parse a MIPS program
 if __name__ == '__main__':
-    f = open("prog2.asm")
+    if (len(sys.argv) != 2):
+        print("Usage: ./pyma <program>")
+        exit();
+    f = open(sys.argv[1])
     p = Parser()
     h = ''
     for line in f:
